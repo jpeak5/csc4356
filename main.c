71236ad00fd322b982d84ee7297e774a051e993b
jason peak <jpeak5@lsu.edu>
2012-09-12 19:29:01 -0500

    changed the counter i init val to make checking easier (1-60), not 0-59

--- main.c
+++ main.c
@@ -15,8 +15,17 @@ int main(int argc, char** argv) {
     int verts,triangles;
     FILE * f = fopen("mesh/icosa.mesh","r");
     fscanf(f,"%2d %2d",&verts, &triangles);
+    
     fprintf(stdout, "hello %s", "World");
     fprintf(stdout, "got %2d verts and %2d triangles", verts, triangles);
+    int i = 1;
+    for(i; i<=verts;i++){
+        float px,py,pz,nx,ny,nz;
+        char c;
+        fscanf(f, "%c     %f     %f     %f     %f     %f     %f", &c, &px, &py, &pz, &nx, &ny, &nz);
+        
+        fprintf(stdout, "%d) line code %c (%f, %f, %f), normal (%f, %f, %f)\n", i, c,px,py,pz,nx,ny,nz);
+    }
     fclose(f);
     return (EXIT_SUCCESS);
 }
