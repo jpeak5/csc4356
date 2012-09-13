44651dda2febd11f17a37cd5c0e04e1a93b391ae
jason peak <jpeak5@lsu.edu>
2012-09-12 20:42:24 -0500

    this is broken- fscanf reads line by line? resets after each call? or what?

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ p1 : main.o
 
 main.o : main.c
 	gcc -c main.c
+
+clean :
+	rm main.o p1
--- main.c
+++ main.c
@@ -14,19 +14,36 @@
 int main(int argc, char** argv) {
     int verts,triangles;
     FILE * f = fopen("mesh/icosa.mesh","r");
-    fscanf(f,"%2d %2d",&verts, &triangles);
-    
-    fprintf(stdout, "hello %s", "World");
-    fprintf(stdout, "got %2d verts and %2d triangles", verts, triangles);
+    fscanf(f,"%d %d",&verts, &triangles);
     int i = 1;
-    for(i; i<=verts;i++){
+    
+        int t1,t2,t3;
+        char c;
+        fscanf(f, "%c %d %d %d", &c, &t1, &t2, &t3);
+        fprintf(stdout, "%d) line code %c (%d, %d, %d)\n", i, c,t1,t2,t3);
+    
+    for(i; i <= verts; i++){
         float px,py,pz,nx,ny,nz;
         char c;
         fscanf(f, "%c     %f     %f     %f     %f     %f     %f", &c, &px, &py, &pz, &nx, &ny, &nz);
         
         fprintf(stdout, "%d) line code %c (%f, %f, %f), normal (%f, %f, %f)\n", i, c,px,py,pz,nx,ny,nz);
     }
+    i = 1;
+
+    /**This is not working!!!
+    for(i; i<=triangles;i++){
+        int t1,t2,t3;
+        char c;
+    */
+        fscanf(f, "%c %d %d %d", &c, &t1, &t2, &t3);
+        fprintf(stdout, "%d) line code %c (%d, %d, %d)\n", i, c,t1,t2,t3);
+    /*
+    }
+    */
+    
     fclose(f);
+    fprintf(stdout, "\n\ngot %d verts and %d triangles\n", verts, triangles);
     return (EXIT_SUCCESS);
 }
 
