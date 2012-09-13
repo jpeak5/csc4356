aa6ecad1b39672a0fe1c44ac7f6bfd21779b8f83
jason peak <jpeak5@lsu.edu>
2012-09-13 05:39:16 -0500

    accounting for newlines really helps keep fscanf on track

--- main.c
+++ main.c
@@ -12,35 +12,31 @@
  * 
  */
 int main(int argc, char** argv) {
+
     int verts,triangles;
     FILE * f = fopen("mesh/icosa.mesh","r");
-    fscanf(f,"%d %d",&verts, &triangles);
+
+    fscanf(f,"%d %d\n",&verts, &triangles);
+    
+    printf("looking for %d verts abd then %d triangles\n\n", verts, triangles);
+    printf("-------------------begin verts------------\n"); 
+    
     int i = 1;
-    
-        int t1,t2,t3;
-        char c;
-        fscanf(f, "%c %d %d %d", &c, &t1, &t2, &t3);
-        fprintf(stdout, "%d) line code %c (%d, %d, %d)\n", i, c,t1,t2,t3);
-    
     for(i; i <= verts; i++){
         float px,py,pz,nx,ny,nz;
         char c;
-        fscanf(f, "%c     %f     %f     %f     %f     %f     %f", &c, &px, &py, &pz, &nx, &ny, &nz);
+        fscanf(f, "%c     %f     %f     %f     %f     %f     %f\n", &c, &px, &py, &pz, &nx, &ny, &nz);
         
-        fprintf(stdout, "%d) line code %c (%f, %f, %f), normal (%f, %f, %f)\n", i, c,px,py,pz,nx,ny,nz);
+        fprintf(stdout, "%02d) %c (%f, %f, %f), normal (%f, %f, %f)\n", i, c,px,py,pz,nx,ny,nz);
     }
     i = 1;
 
-    /**This is not working!!!
     for(i; i<=triangles;i++){
         int t1,t2,t3;
         char c;
-    */
-        fscanf(f, "%c %d %d %d", &c, &t1, &t2, &t3);
-        fprintf(stdout, "%d) line code %c (%d, %d, %d)\n", i, c,t1,t2,t3);
-    /*
+        fscanf(f, "%c %i %i %i\n", &c, &t1, &t2, &t3);
+        fprintf(stdout, "%02d)  %c (%d, %d, %d)\n", i, c,t1,t2,t3);
     }
-    */
     
     fclose(f);
     fprintf(stdout, "\n\ngot %d verts and %d triangles\n", verts, triangles);
