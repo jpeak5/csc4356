7ea8e149f6f0fd8db6f0163054450a8509a816cf
jason peak <jpeak5@lsu.edu>
2012-09-23 08:56:08 -0500

    typo

--- main.c
+++ main.c
@@ -75,7 +75,7 @@ int main(int argc, char** argv) {
     fprintf(stdout, "\n\nexpect size of verts array is %zu\n", verts*glui*6);
     fprintf(stdout, "\n\nsize of verts array is %zu\n", sizeof (v));
 
-    glutInitDisplay(GLUT_DEPTH | GLUT_DOUBLE);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE);
 
     return (EXIT_SUCCESS);
 }
