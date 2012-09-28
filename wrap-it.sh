d52e3446373851ba4b2751a5cc566b38532e59a6
jason peak <jpeak5@lsu.edu>
2012-09-27 23:33:42 -0500

    trivial

--- wrap-it.sh
+++ wrap-it.sh
@@ -3,6 +3,7 @@ ctags *
 git add .
 git commit
 md README.md -f README.html
+git log --pretty=format:'%h, %ar, message: %s' > git-log
 zip p1.zip            docs/* .git/* git-log *.h *.c mesh/* Makefile README.html
 tar -z -cf p1.tar.gz  docs/  .git/  git-log *.h *.c mesh/  Makefile README.html
 
