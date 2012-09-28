353237aa3c4877a724adffedbbec9fecf678b67c
jason peak <jpeak5@lsu.edu>
2012-09-27 23:38:59 -0500

    last housekeeping-only change

--- wrap-it.sh
+++ wrap-it.sh
@@ -4,6 +4,6 @@ git add .
 git commit
 md README.md -f README.html
 git log --pretty=format:'%h, %ar, message: %s' > git-log
-zip p1.zip            docs/* .git/* git-log *.h *.c mesh/* Makefile README.html
-tar -z -cf p1.tar.gz  docs/  .git/  git-log *.h *.c mesh/  Makefile README.html
+zip output/p1.zip            docs/* .git/* git-log *.h *.c mesh/* Makefile README.html .gitignore
+tar -z -cf output/p1.tar.gz  docs/  .git/  git-log *.h *.c mesh/  Makefile README.html .gitignore
 
