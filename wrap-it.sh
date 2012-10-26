efe0871d36026830bcceeaea9039f810cf763cd7
jason peak <jpeak5@lsu.edu>
2012-10-25 22:38:49 -0500

    no need for this foolishness anymore...thanks gh-pages

--- wrap-it.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ctags *
-git add .
-git commit
-md README.md -f README.html
-git log --pretty=format:'%h, %ar, message: %s' > git-log
-zip output/p1.zip            docs/* .git/* git-log *.h *.c mesh/* Makefile README.html .gitignore
-tar -z -cf output/p1.tar.gz  docs/  .git/  git-log *.h *.c mesh/  Makefile README.html .gitignore
-
